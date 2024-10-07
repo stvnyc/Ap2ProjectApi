c4679697debfe14d76f702401c68536df98b1bfe
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-07 01:44:22 -0400

    Agregando tickets al contextto

--- Ap2ProjectApi/DAL/Contexto.cs
+++ Ap2ProjectApi/DAL/Contexto.cs
@@ -8,5 +8,6 @@ public class Contexto : DbContext
     public DbSet<Prioridades> Prioridades { get; set; }
     public DbSet<Sistemas> Sistemas { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Tickets> Tickets { get; set; }
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
