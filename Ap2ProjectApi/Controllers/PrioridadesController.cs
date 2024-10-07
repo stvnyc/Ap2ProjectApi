6accbb05e39265b565b0ff3c6e299397bd62ba06
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-07 02:07:32 -0400

    Modificando Post y Delete

--- Ap2ProjectApi/Controllers/PrioridadesController.cs
+++ Ap2ProjectApi/Controllers/PrioridadesController.cs
@@ -1,108 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Ap2ProjectApi.DAL;
+using Ap2ProjectApi.Data.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Ap2ProjectApi.DAL;
-using Ap2ProjectApi.Data.models;
 
-namespace Ap2ProjectApi.Controllers
+namespace Ap2ProjectApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PrioridadesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PrioridadesController : ControllerBase
+    private readonly Contexto _context;
+    public PrioridadesController(Contexto context)
     {
-        private readonly Contexto _context;
-
-        public PrioridadesController(Contexto context)
+        _context = context;
+    }
+    // GET: api/Prioridades
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Prioridades>>> GetPrioridades()
+    {
+        return await _context.Prioridades.ToListAsync();
+    }
+    // GET: api/Prioridades/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Prioridades>> GetPrioridades(int id)
+    {
+        var prioridades = await _context.Prioridades.FindAsync(id);
+        if (prioridades == null)
         {
-            _context = context;
+            return NotFound();
         }
-
-        // GET: api/Prioridades
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Prioridades>>> GetPrioridades()
+        return prioridades;
+    }
+    // PUT: api/Prioridades/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPrioridades(int id, Prioridades prioridades)
+    {
+        if (id != prioridades.PrioridadId)
         {
-            return await _context.Prioridades.ToListAsync();
+            return BadRequest();
         }
-
-        // GET: api/Prioridades/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Prioridades>> GetPrioridades(int id)
+        _context.Entry(prioridades).State = EntityState.Modified;
+        try
         {
-            var prioridades = await _context.Prioridades.FindAsync(id);
-
-            if (prioridades == null)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!PrioridadesExists(id))
             {
                 return NotFound();
             }
-
-            return prioridades;
+            else
+            {
+                throw;
+            }
         }
-
-        // PUT: api/Prioridades/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPrioridades(int id, Prioridades prioridades)
-        {
-            if (id != prioridades.PrioridadId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(prioridades).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!PrioridadesExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Prioridades
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Prioridades>> PostPrioridades(Prioridades prioridades)
+        return NoContent();
+    }
+    // POST: api/Prioridades
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Prioridades>> PostPrioridades(Prioridades prioridades)
+    {
+        if (prioridades.PrioridadId <= 0 || !PrioridadesExists(prioridades.PrioridadId))
         {
             _context.Prioridades.Add(prioridades);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPrioridades", new { id = prioridades.PrioridadId }, prioridades);
         }
-
-        // DELETE: api/Prioridades/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePrioridades(int id)
+        else
         {
-            var prioridades = await _context.Prioridades.FindAsync(id);
-            if (prioridades == null)
-            {
-                return NotFound();
-            }
-
-            _context.Prioridades.Remove(prioridades);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            _context.Prioridades.Update(prioridades);
         }
-
-        private bool PrioridadesExists(int id)
+        await _context.SaveChangesAsync();
+        return Ok(prioridades);
+    }
+    // DELETE: api/Prioridades/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePrioridades(int id)
+    {
+        if (!_context.Prioridades.Any())
         {
-            return _context.Prioridades.Any(e => e.PrioridadId == id);
+            return NotFound();
         }
+        var prioridades = await _context.Prioridades.FindAsync(id);
+        if (prioridades == null)
+        {
+            return NotFound();
+        }
+        _context.Prioridades.Remove(prioridades);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+    private bool PrioridadesExists(int id)
+    {
+        return _context.Prioridades.Any(e => e.PrioridadId == id);
     }
 }
--- Ap2ProjectApi/Controllers/TicketsController.cs
+++ Ap2ProjectApi/Controllers/TicketsController.cs
@@ -1,108 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Ap2ProjectApi.DAL;
+using Ap2ProjectApi.Data.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Ap2ProjectApi.DAL;
-using Ap2ProjectApi.Data.models;
 
-namespace Ap2ProjectApi.Controllers
+namespace Ap2ProjectApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TicketsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TicketsController : ControllerBase
+    private readonly Contexto _context;
+    public TicketsController(Contexto context)
     {
-        private readonly Contexto _context;
-
-        public TicketsController(Contexto context)
+        _context = context;
+    }
+    // GET: api/Tickets
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Tickets>>> GetTickets()
+    {
+        return await _context.Tickets.ToListAsync();
+    }
+    // GET: api/Tickets/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Tickets>> GetTickets(int id)
+    {
+        var tickets = await _context.Tickets.FindAsync(id);
+        if (tickets == null)
         {
-            _context = context;
+            return NotFound();
         }
-
-        // GET: api/Tickets
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tickets>>> GetTickets()
+        return tickets;
+    }
+    // PUT: api/Tickets/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTickets(int id, Tickets tickets)
+    {
+        if (id != tickets.TicketId)
         {
-            return await _context.Tickets.ToListAsync();
+            return BadRequest();
         }
-
-        // GET: api/Tickets/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Tickets>> GetTickets(int id)
+        _context.Entry(tickets).State = EntityState.Modified;
+        try
         {
-            var tickets = await _context.Tickets.FindAsync(id);
-
-            if (tickets == null)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TicketsExists(id))
             {
                 return NotFound();
             }
-
-            return tickets;
+            else
+            {
+                throw;
+            }
         }
-
-        // PUT: api/Tickets/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTickets(int id, Tickets tickets)
-        {
-            if (id != tickets.TicketId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(tickets).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TicketsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Tickets
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Tickets>> PostTickets(Tickets tickets)
+        return NoContent();
+    }
+    // POST: api/Tickets
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Tickets>> PostTickets(Tickets tickets)
+    {
+        if (tickets.TicketId <= 0 || !TicketsExists(tickets.TicketId))
         {
             _context.Tickets.Add(tickets);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTickets", new { id = tickets.TicketId }, tickets);
         }
-
-        // DELETE: api/Tickets/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTickets(int id)
+        else
         {
-            var tickets = await _context.Tickets.FindAsync(id);
-            if (tickets == null)
-            {
-                return NotFound();
-            }
-
-            _context.Tickets.Remove(tickets);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            _context.Tickets.Update(tickets);
         }
-
-        private bool TicketsExists(int id)
+        await _context.SaveChangesAsync();
+        return Ok(tickets);
+    }
+    // DELETE: api/Tickets/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTickets(int id)
+    {
+        if (!_context.Tickets.Any())
         {
-            return _context.Tickets.Any(e => e.TicketId == id);
+            return NotFound();
         }
+        var tickets = await _context.Tickets.FindAsync(id);
+        if (tickets == null)
+        {
+            return NotFound();
+        }
+        _context.Tickets.Remove(tickets);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+    private bool TicketsExists(int id)
+    {
+        return _context.Tickets.Any(e => e.TicketId == id);
     }
 }
