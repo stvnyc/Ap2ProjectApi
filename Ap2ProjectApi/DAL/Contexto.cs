64eae0d1d7a82d146be6a0b305b96af9e194e468
Steven Yesandy Candelario Fernández <steven_candelario@ucne.edu.do>
2024-10-07 01:43:52 -0400

    Agregando Contexto

---
+++ Ap2ProjectApi/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Ap2ProjectApi.Data.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ap2ProjectApi.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Prioridades> Prioridades { get; set; }
+    public DbSet<Sistemas> Sistemas { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
